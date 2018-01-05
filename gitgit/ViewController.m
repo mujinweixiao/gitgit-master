e012aca3e5017ff4af882bffb35e4278c1507bc1
xiaoxin.cao <1216099190@qq.com>
2018-01-05 15:41:10 +0800

    Initial Commit

---
+++ gitgit/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  gitgit
+//
+//  Created by cying on 2018/1/5.
+//  Copyright © 2018年 saiyunjiuzhou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    NSLog(@"gitgitgitgitgitgitgitgitgitgitgitgitgitgit");
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
